2faba2ed8a79c1188d1d685bc096a71017d8e4b6
Seren_Modz 21 <seren@teamkkb.xyz>
2021-01-19 07:33:12 +0000

    Add version property, update the delete method and more

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © SerenModz21 2018 - 2020 All Rights Reserved.
+ * Copyright © SerenModz21 2018 - 2021 All Rights Reserved.
  * Unauthorized distribution of any code within this project may result in consequences chosen by the Board Members.
  * Refer to the README for more information.
  */
@@ -8,9 +8,14 @@ import fetch from "node-fetch"
 import { PasteGGOptions, PasteOutput, PostPaste, IHeader, UpdatePost } from "./interfaces"
 
 class PasteGG {
+  /** Auth key for paste.gg API */
   readonly #auth: string;
+  /** The full URL for the API */
   readonly #url: string;
+  /** The base URL and API version used */
   readonly options: PasteGGOptions;
+  /** The current version of the API wrapper */
+  readonly version: string;
 
   /**
    * Create a new instance of PasteGG
@@ -24,9 +29,17 @@ class PasteGG {
   }) {
     this.#auth = auth;
     this.options = options;
+    this.version = require("../package.json").version;
     this.#url = `${this.options.baseUrl}/v${this.options.version}`;
   }
 
+  /**
+   * The headers required for the request
+   * @param {string} auth
+   * @param {boolean} content
+   * @returns {IHeader}
+   * @private
+   */
   private headers(auth?: string, content?: boolean): IHeader {
     const headers: IHeader = {}
 
@@ -42,6 +55,7 @@ class PasteGG {
    * @param {string} id The ID of the paste.
    * @param {boolean} full Includes the contents of files if true.
    * @returns {Promise<PasteOutput>}
+   * @public
    */
   public async get(id: string, full: boolean = false): Promise<PasteOutput> {
     const res = await fetch(`${this.#url}/pastes/${id}?full=${full}`, {
@@ -56,6 +70,7 @@ class PasteGG {
    * Create a new paste.
    * @param {PostPaste} input The information to create the paste with.
    * @returns {Promise<PasteOutput>}
+   * @public
    */
   public async post(input: PostPaste): Promise<PasteOutput> {
     const res = await fetch(`${this.#url}/pastes`, {
@@ -70,13 +85,14 @@ class PasteGG {
   /**
    * Deletes an existing paste.
    * @param {string} id The ID of the paste to delete.
-   * @param {string} key Auth key or deletion key, enter "auth" for auth key
-   * @returns {Promise<void | PasteOutput>}
+   * @param {string} [key] Auth key or deletion key (leave blank if you have set the auth key in the constructor)
+   * @returns {Promise<PasteOutput | void>}
+   * @public
    */
-  public async delete(id: string, key: string): Promise<void | PasteOutput> {
+  public async delete(id: string, key?: string): Promise<PasteOutput | void> {
     const res = await fetch(`${this.#url}/pastes/${id}`, {
       method: "DELETE",
-      headers: this.headers(key?.toLowerCase() !== "auth" ? key : null)
+      headers: this.headers(key)
     })
 
     return res.json()
@@ -86,9 +102,10 @@ class PasteGG {
    * Update an existing paste.
    * @param {string} id The ID for the paste to update.
    * @param {UpdatePost} options The options you wish to update.
-   * @returns {Promise<void | PasteOutput>}
+   * @returns {Promise<PasteOutput | void>}
+   * @public
    */
-  public async update(id: string, options: UpdatePost): Promise<void | PasteOutput> {
+  public async update(id: string, options: UpdatePost): Promise<PasteOutput | void> {
     if (!this.#auth) throw new Error("An auth key is required for this endpoint!");
 
     const res = await fetch(`${this.#url}/pastes/${id}`, {
